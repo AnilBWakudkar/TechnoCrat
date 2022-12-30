6976df5d55c2d3a7bf9bf625a1cbe36818740f58
Sakshi <ingolesakshi1@gmail.com>
2022-12-29 21:59:58 -0800

    update site

--- technocrat/src/App.js
+++ technocrat/src/App.js
@@ -1,12 +1,26 @@
-
-import './App.css';
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Expertise from "./Components/Expertise";
+import Solutions from  "./Components/Solutions";
+import ThoughtCenter from "./Components/ThoughtCenter";
+import Contact from "./Components/Contact";
+import Joinus from "./Components/Joinus";
+import{Routes,Route} from "react-router-dom"; 
+import  Style from './Components/';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      
+      <Home/>
+      <Routes>
+      <Route path="/" exact element={<Home/>} />
+      <Route path="/about"  element={<About/>} />
+
+
+
+
+      </Routes>
     </div>
   );
 }
